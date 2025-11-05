c09524b4efe912d7df2609f210a85f1c203e1338
tiffababu <pjm199791@gmail.com>
2025-11-05 09:48:06 +0800

    Add water sensor ISR, but need modify the trigger

--- CG2271_Project.c
+++ CG2271_Project.c
@@ -74,11 +74,14 @@ static void gpio_init_led(void)
 }
 
 /* ------------------------------ GPIO/Water Sensor -------------------------------- */
+// Initialize water sensor pins and interrupt
 static void init_watersensor() {
+    NVIC_DisableIRQ(PORTC_PORTD_IRQn);
+
 	//activate PORTC
 	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
 
-	//select GPIO for all pins
+	//configure as GPIO for all pins
 	PORTC->PCR[WATERSENSORSIGNAL] &= ~PORT_PCR_MUX_MASK;
 	PORTC->PCR[WATERSENSORSIGNAL] |= PORT_PCR_MUX(1);
 
@@ -88,7 +91,7 @@ static void init_watersensor() {
 	PORTC->PCR[WATERSENSORGND] &= ~PORT_PCR_MUX_MASK;
 	PORTC->PCR[WATERSENSORGND] |= PORT_PCR_MUX(1);
 
-	//Signal to input, VCC and GND to output
+	//configure signal to input, VCC and GND to output
 	GPIOC->PDDR &= ~(1 << WATERSENSORSIGNAL);
 	GPIOC->PDDR |= (1 << WATERSENSORVCC);
 	GPIOC->PDDR |= (1 << WATERSENSORGND);
@@ -96,6 +99,30 @@ static void init_watersensor() {
 	//Set VCC HIGH and GND LOW
 	GPIOC->PSOR |= (1 << WATERSENSORVCC);
 	GPIOC->PCOR &= ~(1 << WATERSENSORGND);
+
+    // configure IRQ type to trigger on falling edge
+    PORTC->PCR[WATERSENSORSIGNAL] &= ~PORT_PCR_IRQC_MASK;
+    PORTC->PCR[WATERSENSORSIGNAL] |= PORT_PCR_IRQC(0b1010);  // falling edge
+
+    PORTC->ISFR |= (1<< WATERSENSORSIGNAL); // clear any pending interrupt
+
+    NVIC_SetPriority(PORTC_PORTD_IRQn, 64);
+    NVIC_ClearPendingIRQ(PORTC_PORTD_IRQn);
+    NVIC_EnableIRQ(PORTC_PORTD_IRQn);
+}
+
+// Interrupt handler for PORTC 
+void PORTC_PORTD_IRQHandler() {
+    NVIC_ClearPendingIRQ(PORTC_PORTD_IRQn);
+    
+    if (PORTC->ISFR & (1 << WATERSENSORSIGNAL)) {
+        PRINTF("Water sensor triggered interrupt!\r\n"); //need check cos it's when there's no water detected by water sensor
+        // Toggle red LED as indication to top up water tank
+        GPIOE->PTOR |= (1 << REDLED);
+    }
+
+    // clear interrupt flag
+    PORTC->ISFR |= (1<< WATERSENSORSIGNAL);
 }
 
 /* ----------------------------- ADC0 setup ------------------------------- */
